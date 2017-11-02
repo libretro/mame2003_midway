9a7e92756829f93e36aab6f25a3597bf9ade4ab2
gamez-fan <sy@sy1873.plus.com>
2017-11-02 21:52:56 +0000

    Fix random crash in Rampage World Tour
    This fix from MAME114 will stop the game randomly crashing at the end of a level

--- src/vidhrdw/midtunit_vidhrdw.c
+++ src/vidhrdw/midtunit_vidhrdw.c
@@ -644,7 +644,12 @@ static void dma_callback(int is_in_34010_context)
 
 READ16_HANDLER( midtunit_dma_r )
 {
-	return dma_register[offset];
+	/* rmpgwt sometimes reads register 0, expecting it to return the */
+	/* current DMA status; thus we map register 0 to register 1 */
+	/* openice does it as well */
+	if (offset == 0)
+		offset = 1;
+ 	return dma_register[offset];
 }
 
 
